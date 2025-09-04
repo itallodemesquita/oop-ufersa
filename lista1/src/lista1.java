2b9d56ec134bbfd094b45db077eb3b47236b4057
itallodemesquita <itallodemesquita@gmail.com>
2025-09-04 20:34:15 -0300

    "questao2" code in Java

--- lista1/src/lista1.java
+++ lista1/src/lista1.java
@@ -11,14 +11,31 @@ public class lista1 {
         System.out.print("Digite a quantidade maxima da peça: ");
         int maxima = input.nextInt();
 
-        double estoqueMedio = (minima + maxima) / 2;
+        double estoqueMedio = (minima + maxima) / 2;    
 
-        System.out.printf("\nO estoque medio da peça é %f", estoqueMedio);
+        System.out.printf("\nO estoque medio da peça é %.2f", estoqueMedio);
 
         input.close();
 
     }
+
+    public static void questao2(){
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Qual é a cotação atual do dólar? ");
+        float cotacao = input.nextFloat();
+
+        System.out.print("Quantos dólares você possui? ");
+        float saldoDolar = input.nextFloat();
+
+        float saldoReal = cotacao * saldoDolar;
+
+        System.out.printf("Sua quantia em dólares equivale a R$ %f.", saldoReal);
+
+        input.close();
+    }
+
     public static void main(String[] args) {
-        questao1();
+        questao2();
     }
 }
