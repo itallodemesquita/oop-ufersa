691b4fda6d894f5b7fdd6ee714b4c5c6d6e5239c
itallodemesquita <itallodemesquita@gmail.com>
2025-09-04 21:08:37 -0300

    "questao3" code in Java

--- lista1/src/lista1.java
+++ lista1/src/lista1.java
@@ -35,7 +35,30 @@ public class lista1 {
         input.close();
     }
 
+    public static void questao3(){
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Vendedor, digite o seu ID: ");
+        int id = input.nextInt();
+
+        System.out.print("Digite o código da peça: ");
+        int idPeca = input.nextInt();
+
+        System.out.print("Qual é o preço da peça?: ");
+        float valorPeca = input.nextFloat();
+
+        System.out.print("Quantas unidades foram vendidas? ");
+        int qtd = input.nextInt();
+
+        float total = valorPeca * qtd;
+
+        System.out.printf("\nValor total da venda da peça %d: %f\n", idPeca, total);
+        System.out.printf("Vendedor %d, sua comissão é de R$ %.2f.\n", id, (total * 0.05));
+
+        input.close();
+    }
+
     public static void main(String[] args) {
-        questao2();
+        questao3();
     }
 }
